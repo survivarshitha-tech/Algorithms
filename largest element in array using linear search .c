bd76d1f886720854858d2a7109dfc0ce490cb67a
survivarshitha-tech <survivarshitha@gmail.com>
2026-03-09 10:25:37 +0530

    Add files via upload

---
+++ largest element in array using linear search .c
@@ -0,0 +1,25 @@
+//Find largest element in array using linear search 
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, max;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    max = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    printf("Largest element = %d", max);
+
+    return 0;
+}
