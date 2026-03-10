8c32769f0d3f53ffc2fbe40eab44869c6b478a7e
survivarshitha-tech <survivarshitha@gmail.com>
2026-03-10 12:18:30 +0530

    Add files via upload

---
+++ reverse an array using swaping algorithm .c
@@ -0,0 +1,26 @@
+//Reverse an array using swaping algorithm 
+
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n/2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+    }
+
+    printf("Reversed array:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
